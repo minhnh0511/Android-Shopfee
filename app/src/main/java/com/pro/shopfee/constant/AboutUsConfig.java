5a8a79395ce20da706e4d505cbf77b05d3e07a14
minhnh <huuminh0511@gmail.com>
2025-03-25 00:39:48 +0700

    fix config info

--- app/src/main/java/com/pro/shopfee/constant/AboutUsConfig.java
+++ app/src/main/java/com/pro/shopfee/constant/AboutUsConfig.java
@@ -1,14 +1,14 @@
 package com.pro.shopfee.constant;
 
 public interface AboutUsConfig {
-    String ABOUT_US_TITLE = "TinCoder";
-    String ABOUT_US_CONTENT = "Provides Android, iOS, Web Source Codes and Development Services. " +
+    String ABOUT_US_TITLE = "Shopfee";
+    String ABOUT_US_CONTENT = "Shopfee - Bring you good drinks, developed by minhnhhe170924 " +
             "Highly Committed to Quality Products and Excellent Services.";
     String ABOUT_US_SLOGAN = "Highly committed to quality products & excellent services";
-    String ABOUT_US_WEBSITE_TITLE = "Website TinCoder";
+    String ABOUT_US_WEBSITE_TITLE = "FPT/PRM392/minhnhhe170924";
 
     String PAGE_FACEBOOK = "fb://page/tincoder";
-    String LINK_FACEBOOK = "https://www.facebook.com/tincoder";
+    String LINK_FACEBOOK = "https://www.facebook.com";
     String LINK_YOUTUBE = "https://www.youtube.com/channel/UCu3DXfXYgygIYXN8TduNcNQ";
     String PHONE_NUMBER = "+84392229676";
     String GMAIL = "mixproduction29932@gmail.com";
